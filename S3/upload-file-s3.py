a7a6cdd721c3b965a94b45c6a4ef4641b3a24088
Zafer Aktekin <94222417+ZaferAktekin@users.noreply.github.com>
2025-10-05 13:11:36 +0000

    update upload-file-s3.py

--- S3/upload-file-s3.py
+++ S3/upload-file-s3.py
@@ -3,3 +3,11 @@
 import boto3
 
 s3 = boto3.client('s3')
+
+# Specify a file to upload in s3
+
+filename = 'test-boto3.txt' # File name to be uploaded
+bucket_name = '' # The bucket name that the file will be uploaded
+
+
+s3.upload_file(filename, bucket_name, modify_name)
