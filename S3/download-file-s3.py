453233f110e5eba4e4e4106ee47bf5a7ee7cbf9d
Zafer Aktekin <94222417+ZaferAktekin@users.noreply.github.com>
2025-10-08 12:28:12 +0000

    update download-file.py

--- S3/download-file-s3.py
+++ S3/download-file-s3.py
@@ -3,3 +3,6 @@
 import boto3
 
 s3 = boto3.client('s3')
+
+filename = 'test-boto3.txt' # File name to be downloaded
+bucket_name = 'test-bucket-zafer' # The bucket name that the file will be downloaded
