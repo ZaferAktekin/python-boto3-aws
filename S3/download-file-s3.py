6911583f3b90b6249ee0a2a56ec28d00fc942521
Zafer Aktekin <94222417+ZaferAktekin@users.noreply.github.com>
2025-10-08 12:29:02 +0000

    update download-file.py

--- S3/download-file-s3.py
+++ S3/download-file-s3.py
@@ -4,5 +4,10 @@ import boto3
 
 s3 = boto3.client('s3')
 
-filename = 'test-boto3.txt' # File name to be downloaded
-bucket_name = 'test-bucket-zafer' # The bucket name that the file will be downloaded
+filename = '' # File name to be downloaded
+bucket_name = '' # The bucket name that the file will be downloaded
+
+# Optional
+modify_name = '' # Modifies the file name when downloading to the directory from s3
+
+s3.download_file(bucket_name, filename, modify_name)
