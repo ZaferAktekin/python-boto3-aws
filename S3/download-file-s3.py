bb575886146ab4faa2dadaaaad530c2480488f9c
Zafer Aktekin <94222417+ZaferAktekin@users.noreply.github.com>
2025-10-08 12:29:18 +0000

    update download-file.py

--- S3/download-file-s3.py
+++ S3/download-file-s3.py
@@ -11,3 +11,5 @@ bucket_name = '' # The bucket name that the file will be downloaded
 modify_name = '' # Modifies the file name when downloading to the directory from s3
 
 s3.download_file(bucket_name, filename, modify_name)
+
+print(f"{modify_name} has been Downloaded from the source bucket: {bucket_name} as orginal file name: {filename}")
